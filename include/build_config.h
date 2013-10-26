92bf34cdd12d2ad34897c3a32f35ba48afa2c594
Christian Gunderman <cdg46@case.edu>
2013-10-25 21:40:34 -0400

    Updated build_config.h comments to c89 C style. Signed-off-by: Christian Gunderman <cdg46@case.edu>

--- include/build_config.h
+++ include/build_config.h
@@ -1,9 +1,9 @@
 /**
  * Gunderman Data Structs
  * (C) 2013 Christian Gunderman
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
+ * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,10 +12,10 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this program.  If not, see 
+ * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/>.
  *
- * Contact Email: gundermanc@gmail.com 
+ * Contact Email: gundermanc@gmail.com
  */
 #ifndef BUILD__CONFIG__H__
 #define BUILD__CONFIG__H__
@@ -24,7 +24,13 @@
 #include <stdbool.h>
 
 
-// build configuration parameters
+/* Build Configuration parameters:
+ * The following definitions can be used to enable or disable features of
+ * the datastructures. The advantage of this is that it allows you to minimize
+ * memory waste in each linkedlist, stack, or hashtable node due to the UNION. By
+ * commenting out the enables for larger types, such as DOUBLE, LONG, and POINTER,
+ * you shrink the UNION's minimum size from 64 bits, down to 32 and so on.
+ */
 #define DATASTRUCT_ENABLE_BOOL
 #define DATASTRUCT_ENABLE_DOUBLE
 #define DATASTRUCT_ENABLE_LONG
@@ -34,36 +40,36 @@
 #define DATASTRUCT_ENABLE_POINTER
 
 
-// standard data union for all data structures
+/* standard data union for all data structures */
 typedef union {
 
 #ifdef DATASTRUCT_ENABLE_BOOL
   bool boolVal;
-#endif // DATASTRUCT_ENABLE_BOOL
+#endif /* DATASTRUCT_ENABLE_BOOL */
 
 #ifdef DATASTRUCT_ENABLE_DOUBLE
   double doubleVal;
-#endif // DATASTRUCT_ENABLE_DOUBLE
+#endif /* DATASTRUCT_ENABLE_DOUBLE */
 
 #ifdef DATASTRUCT_ENABLE_LONG
   long longVal;
-#endif // DATASTRUCT_ENABLE_LONG
+#endif /* DATASTRUCT_ENABLE_LONG */
 
 #ifdef DATASTRUCT_ENABLE_INT
   int intVal;
-#endif // DATASTRUCT_ENABLE_INT
+#endif /* DATASTRUCT_ENABLE_INT */
 
 #ifdef DATASTRUCT_ENABLE_SHORT
   short shortVal;
-#endif // DATASTRUCT_ENABLE_SHORT
+#endif /* DATASTRUCT_ENABLE_SHORT */
 
 #ifdef DATASTRUCT_ENABLE_CHAR
   char charVal;
-#endif // DATASTRUCT_ENABLE_CHAR
+#endif /* DATASTRUCT_ENABLE_CHAR */
 
 #ifdef DATASTRUCT_ENABLE_POINTER
   void * pointerVal;
-#endif // DATASTRUCT_ENABLE_POINTER
+#endif /* DATASTRUCT_ENABLE_POINTER */
 
 }DSValue;
 
